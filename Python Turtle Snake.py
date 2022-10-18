3ca783c2d803aa3fae082a20844252183bcf21b7
turtleking357 <69770434+turtleking357@users.noreply.github.com>
2022-10-18 21:27:15 +0100

    Python Turtle Game
    First time running will currently give an error as it reads the high score file before creating it and putting 0 in it, once you run it the 2nd time, it'll work

---
+++ Python Turtle Snake.py
@@ -0,0 +1,213 @@
+import turtle
+import time
+import random
+#checking if HighScore.txt is empty
+HSWrite = open("HighScore.txt","a")
+HSWrite.close()
+HSRead = open("HighScore.txt","r")
+high_score = HSRead.readline()
+print("Current High score is:", high_score)
+HSRead.close()
+if high_score != 0:
+    HSAppend = open("HighScore.txt","a")
+    HSAppend.write(" 1")
+    HSAppend.close()
+    HSRead = open("HighScore.txt","r")
+    highscore = HSRead.readline()
+    HSRead.close()
+    if highscore[:2] == " 1":
+        HSWrite = open("HighScore.txt","w")
+        HSWrite.write("0")
+        HSRead.close()
+    else:
+        HSWrite = open("HighScore.txt","w")
+        HSWrite.write(high_score)
+        HSWrite.close()
+#Setting Starting Delay
+delay = 0.1
+
+# Score
+score = 0
+
+#open high score file
+HighScore = open("HighScore.txt","r")
+high_score = int(HighScore.readline())
+HighScore.close()
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game By Luke")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0) # Turns off the screen updates
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("red")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
+# Functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Check for a collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+        # Update high score
+        HighScore = open("HighScore.txt", "w")
+        HighScore.write(str(high_score))
+        HighScore.close()
+
+
+    # Check for a collision with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a tail
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("#580000")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()
+
+    # Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+            # Reset the delay
+            delay = 0.1
+
+            # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+            #Update high score
+            HighScore = open("HighScore.txt","w")
+            HighScore.write(str(high_score))
+            HighScore.close()
+
+    time.sleep(delay)
